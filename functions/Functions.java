44fed9fda8aee75efd34fc06983b9d644fa0c5a3
OstrovskyYura <126604893+OstrovskyYura@users.noreply.github.com>
2023-04-12 19:51:06 +0300

    Add files via upload

---
+++ functions/Functions.java
@@ -0,0 +1,120 @@
+package functions;
+
+import patients.Patient;
+import java.util.stream.*;
+import java.util.*;
+
+public class Functions {
+    public void addPatient(List<Patient> list, String fullName, String address, int phone, int medicalCardNumber, String diagnosis) {
+        Patient patient = new Patient(fullName, address, phone, medicalCardNumber, diagnosis);
+        list.add(patient);
+    }
+    public void removePatient(int patientToDelete, List<Patient> list) {
+        if (list.isEmpty()) {
+            return;
+        }
+        boolean remove = list.removeIf(patient -> patient.getId() == patientToDelete);
+        if (!remove) {}
+    }
+
+    public void showAllPatients(List<Patient> list) {
+        if (list.isEmpty()) {
+            return;
+        }
+        list.forEach(System.out::println);
+    }
+
+    public void showAllDiagnosisPatients(List<Patient> list) {
+        if (list.isEmpty()) {
+            return;
+        }
+        List<String> diagnoses = list.stream()
+                .map(Patient::getDiagnosis)
+                .distinct().collect(Collectors.toList());
+        System.out.println(diagnoses);
+
+//        Set<String> diagnoses = new HashSet<>();
+//        diagnoses.add("flu");
+//        diagnoses.add("cold");
+//        diagnoses.add("cancer");
+//        diagnoses.add("flu");
+//        diagnoses.add("cold");
+//        diagnoses.add("heart disease");
+//        for (String diagnosis : diagnoses) {
+//            System.out.println(diagnosis);
+//        }
+    }
+    public List<Patient> searchAndSortMedicalCardNumber(String diagnosis, List<Patient> list) {
+        if (list.isEmpty()) {
+            return null;
+        }
+        List<Patient> tempList = list.stream()
+                .filter(p -> p.getDiagnosis().equals(diagnosis))
+                .sorted(Comparator.comparingInt(Patient::getMedicalCardNumber))
+                .collect(Collectors.toList());
+        Collections.sort(list, Comparator.comparing(Patient::getMedicalCardNumber));
+        return tempList;
+    }
+
+    public List<Patient> searchFullNameAndPhone(int startCardNumber, List<Patient> list, int endCardNumber) {
+        if (list.isEmpty()) {
+            return null;
+        }
+        List<Patient> tempList = list.stream()
+                .filter(p -> p.getMedicalCardNumber() >= startCardNumber && p.getMedicalCardNumber() <= endCardNumber)
+                .collect(Collectors.toList());
+        return tempList;
+    }
+
+    public List<Patient> foundPatients(String phonePrefix, List<patients.Patient> list) {
+        if (list.isEmpty()) {
+            return null;
+        }
+        List<Patient> tempList = list.stream()
+                .filter(p -> p.getPhone().charAt(0) == phonePrefix)
+                .collect(Collectors.toList());
+//        List<patients.Patient> tempList = new ArrayList<>();
+//        list.forEach(patient -> {
+//                if (Integer.toString(patient.getPhone()).startsWith(phonePrefix)) {
+//                    tempList.add(patient);
+//                }
+//        });
+        return tempList;
+    }
+
+    public boolean inputValidate(String input) {
+        if (input.contains(" ") || input.contains("\n") ||
+                input.contains("\t") || input.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    public List<Patient> ListOfPatientDiagnoses(List<Patient> list){
+        Map<String, Integer> diagnosisCount = new HashMap<>();
+        for (Patient patient : list) {
+            String diagnosis = patient.getDiagnosis();
+            diagnosisCount.put(diagnosis, diagnosisCount.getOrDefault(diagnosis, 0) + 1);
+        }
+        diagnosisCount.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .forEach(entry -> System.out.printf("%s - %d пацієнтів\n", entry.getKey(), entry.getValue()));
+        return null;
+    }
+
+    public static <T> List<T> convertToList(T[] arr)
+    {
+        List<T> list = new ArrayList<>();
+        for (T i: arr) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    public static List<Patient> fullNameSort(List<Patient> list){
+        Map<String, Long> countByDiagnosis = list.stream()
+                .collect(Collectors.groupingBy(Patient::getDiagnosis, Collectors.counting()));
+        System.out.println(countByDiagnosis);
+        return list;
+    }
+}
---
+++ info/Info.java
@@ -0,0 +1,57 @@
+package info;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+public class Info {
+    private boolean running = false;
+    private List<InfoItem> infoItems = new ArrayList<>();
+    public Info(List<InfoItem> infoItems) {
+        this.infoItems.addAll(infoItems);
+    }
+    public void show() {
+        for (int i = 0; i < infoItems.size(); i++) {
+            System.out.println(i + " - " + infoItems.get(i).getName());
+        }
+    }
+
+    public int getInput() {
+        int select = 0;
+        Scanner scanner = new Scanner(System.in);
+        boolean validInput = false;
+        do {
+            System.out.println("Оберіть функцію: ");
+            try {
+                select = scanner.nextInt();
+                scanner.nextLine();
+                if (select > infoItems.size() - 1 || select < 0) {
+                    System.out.println("Будь-ласка оберіть те, що є у списку!");
+                } else {
+                    validInput = true;
+                }
+            } catch (InputMismatchException err) {
+                System.out.println("Будь-ласка оберіть те, що є у списку!");
+                scanner.nextLine();
+            }
+        } while (!validInput);
+        return select;
+    }
+
+    public void perform(int select) {
+        running = true;
+        this.infoItems.get(select).runMethod();
+        if (!running){
+            System.exit(0);
+        }
+        System.out.println();
+    }
+    public void run() {
+        int select;
+        running = true;
+        while (running) {
+            show();
+            select = getInput();
+            perform(select);
+        }
+    }
+}
